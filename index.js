030e1d0b786c36b46f31a2932d55a274e3bdfb7b
Mouaz Jamal <mj225ih@student.lnu.se>
2026-01-15 17:50:50 +0100

    Create new ejs files in folder views and render them and starting a server with a port

--- index.js
+++ index.js
@@ -3,5 +3,35 @@ const path = require('path')
 
 const app = express()
 
-app.set('view engine', ejs)
+app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+
+const messages = [
+    {
+        text: 'Hi nice to meet you!',
+        user: 'Maxi',
+        added: new Date()
+    }, 
+    {
+        text: 'Hello there!',
+        user: 'Lisa',
+        added: new Date()
+    }
+]
+
+app.get('/', (req, res) => {
+  res.render('main')
+})
+
+app.get('/new', (req, res) => {
+    res.render('messages')
+})
+
+const PORT = 3000
+
+app.listen(PORT, (err) => {
+    if (err) {
+        throw new TypeError('Something went wrong')
+    }
+    console.log(`Server running on port ${PORT}`)
+})
