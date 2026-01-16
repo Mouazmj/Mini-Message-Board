ed019233ca1b72868682d695cfd608d5a474f151
Mouaz Jamal <mj225ih@student.lnu.se>
2026-01-16 17:48:55 +0100

    Activate req.body so I can use POST method

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const path = require('path')
-const { title } = require('process')
+
 
 const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+app.use(express.urlencoded({ extended: true }));
 
 const messages = [
     {
